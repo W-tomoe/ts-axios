d9691e870fa51d8944368d5b6273aaef5d3209fe
wong <375640259@qq.com>
2019-07-01 19:09:50 +0800

    'base'

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
 import { AxiosRequestConfig } from './typs'
 import xhr from './xhr'
+import { buildURL } from './helpers/url';
+import { transformRequest, transformResponse } from './helpers/data'
+
+import { proccessHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig) {
-    xhr(config)
+    processConfig(config)
+    return xhr(config).then(res => {
+        return transformResponseData(res)
+    })
+}
+
+function processConfig(config: AxiosRequestConfig): void{
+    config.url = transformUrl(config)
+    config.headers = transformHeaders(config)
+    config.data = transformRequestData(config)
+}
+
+function transformUrl(config: AxiosRequestConfig): string {
+    const { url, params } = config
+    return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any{
+    return transformRequest(config.data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+    res.data = transformResponse(res.data)
+    return res
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+    const { headers = {}, data } = config
+    return proccessHeaders(headers, data)
 }
 
 export default axios
